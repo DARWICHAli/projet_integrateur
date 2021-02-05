0de5da88949eeaf42c9fd5c73309e732a5e7c209
ESSALHI Jameldin <jameldin.essalhi@etu.unistra.fr>
2021-02-05 10:53:11 +0100

    Debut de scripting plateau + pion

---
+++ godot/Scripts/Plateau.gd
@@ -0,0 +1,19 @@
+extends TileMap
+class_name Plateau
+
+var nb_joueurs
+var cases = []
+var coord #Vector2
+var soldeCagnotte
+
+func _init():
+	nb_joueurs = 0 #temp
+	pass
+
+func _ready():
+	pass 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
