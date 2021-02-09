d6fcf5fd9b4e075ead6789d99914e8a17b2005ad
ESSALHI Jameldin <jameldin.essalhi@etu.unistra.fr>
2021-02-09 15:21:39 +0100

    modif

---
+++ godot/Scripts/AllerPrison.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+class_name allerprison
+
+func _ready():
+	pass
+
+func _on_AllerPrison_body_entered(body):
+	print("VA EN PRISON !")
--- godot/Scripts/Pion.gd
+++ godot/Scripts/Pion.gd
@@ -1,6 +1,8 @@
 extends RigidBody2D
-class_name Pion
 
+signal goPrison
+
+class_name Pion
 
 var motion = Vector2()
 var solde
@@ -10,7 +12,5 @@ func get_solde():
 	#pass
 
 func _physics_process(delta):
-	motion.x = 100
-	motion.y = 100
 	pass
 	
