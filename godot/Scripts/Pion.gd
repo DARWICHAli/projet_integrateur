88c24bf499411b434dfc435129023caf7e67b1d2
ESSALHI Jameldin <jameldin.essalhi@etu.unistra.fr>
2021-02-09 14:01:27 +0100

    Maj scripts sur fonctionnel

---
+++ godot/Scripts/Pion.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+class_name Pion
+
+
+var motion = Vector2()
+var solde
+
+func get_solde():
+	return solde
+	#pass
+
+func _physics_process(delta):
+	motion.x = 100
+	motion.y = 100
+	pass
+	
