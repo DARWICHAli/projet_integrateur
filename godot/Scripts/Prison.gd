1a16d95d81766f9db678a935804fbeea333333a2
ESSALHI Jameldin <jameldin.essalhi@etu.unistra.fr>
2021-02-09 17:11:44 +0100

    Detection des cases de base prototype

---
+++ godot/Scripts/Prison.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+func _ready():
+	pass 
+
+func _on_Prison_body_entered(body):
+	print("VOUS ETES EN PRISON HAHAHAHA")
+
---
+++ godot/Scripts/Start.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+func _ready():
+	pass 
+
+func _on_Start_body_entered(body):
+	print("RECEVEZ 200 ECTS")
+	
