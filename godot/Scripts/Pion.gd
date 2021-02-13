c549542f6447177f3232e20c461803ca49b40d56
debenne <thomas.debenne@etu.unistra.fr>
2021-02-13 15:04:21 +0100

    Début de déplacement par case

--- godot/Scripts/Pion.gd
+++ godot/Scripts/Pion.gd
@@ -2,44 +2,43 @@ extends KinematicBody2D
 
 export (int) var SPEED
 var side = 30
+
+var case_side = 96.0
+var deplacement #en nombre de cases
+
+
 var velocity = Vector2()
-var game_size = Vector2()
+var game_size
 var x_counter = 0
 var y_counter = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var initialPosition = Vector2(382.912,254.332)
+	var initialPosition = Vector2(400,450)
 	position = initialPosition
-	game_size.x = side # Taille de l'écran,à changer par taille du plateau
-	game_size.y = side
-	lancer_de()
+	game_size = get_viewport_rect().size # Taille de l'écran,à changer par taille du plateau
+	deplacement = 1
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	velocity = Vector2()
-	if Input.is_mouse_button_pressed( 1 ): # Clic gauche
+	if ((deplacement * case_side) > 0):
 		if ((x_counter == 0) && (y_counter >= 0) && (y_counter < side)):
 			y_counter+=1
 			velocity.y-=1
 		elif ((x_counter >= 0) && (x_counter < side) && (y_counter == side)):
 			x_counter+=1
-			velocity.x-=1
+			velocity.x+=1
 		elif ((x_counter == side) && (y_counter <= 100) && (y_counter > 0)):
 			y_counter-=1
 			velocity.y+=1
 		elif ((x_counter <= side) && (x_counter > 0) && (y_counter == 0)) :
 			x_counter-=1
-			velocity.x+=1
+			velocity.x-=1
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED
+		deplacement -= (1/case_side)
 	position += velocity * delta
 	position.x=clamp(position.x, 0, game_size.x)
 	position.y=clamp(position.y, 0, game_size.y)
 
-func lancer_de():
-	var rand = RandomNumberGenerator.new()
-	rand.randomize()
-	var deplacement = rand.randi_range(1, 6)
-	print(deplacement)
-	pass
-	
