6f16214b7fce1217ee363cc0bb14318ec720d82f
Neiko DAMOI <neiko.damoi@etu.unistra.fr>
2021-02-16 15:55:15 +0100

    Implementation acceuil et abandon

---
+++ godot/Acceuil.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+func init_game():
+	pass
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	init_game()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_StartButton_pressed():
+	init_game()
+	$"Acceuil".hide()
+
+
+func _on_TextureButton_pressed():
+	init_game()
+	$"Acceuil".show()
+	
