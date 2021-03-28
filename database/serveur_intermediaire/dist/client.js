42d1a8d2f05da65a2dc750a33c2a46d12a878512
debenne <thomas.debenne@etu.unistra.fr>
2021-03-28 16:28:18 +0200

    Suppression du dossier dist, créé automatiquement à la compilation

--- database/serveur_intermediaire/dist/client.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const ws_1 = __importDefault(require("ws"));
-var sock = new ws_1.default("ws://localhost:1234");
-//# sourceMappingURL=client.js.map
