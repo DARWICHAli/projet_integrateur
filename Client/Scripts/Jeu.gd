0df001f94e226980a3fea27121cfbee4868d1ca3
m.fessler <mathieu.fessler@etu.unistra.fr>
2021-04-24 15:19:52 +0200

    Ajout de la suppression des pions, problèmes lors du hide à discuter

--- Client/Scripts/Jeu.gd
+++ Client/Scripts/Jeu.gd
@@ -235,6 +235,9 @@ func _on_data_partie ():
 					emit_signal("signal_resultat_lancer_de7", int(obj.data))
 				7:
 					emit_signal("signal_resultat_lancer_de8", int(obj.data))
+		Structure.PacketType.CACHE_JOUEUR:
+			print("Suppression du joueur %d." % obj.data)
+			supprimer_joueur(obj.data)
 		_:
 			print('autre paquet reçu')
 
@@ -371,5 +374,10 @@ func _on_connexion_connection():
 	structure.set_requete_connexion(mail,mdp)
 	envoyer_message(client_lobby, structure.to_bytes())
 
-	
-
+func supprimer_joueur(n_pion):
+	if n_pion==0:
+		get_node("Pion").hide()
+		$"info_joueur/ScrollContainer/VBoxContainer/infobox1/montant".text = "-1"
+	else:
+		get_node("Pion"+str(n_pion+1)).hide()
+		get_node("info_joueur/ScrollContainer/VBoxContainer/infobox"+ str(n_pion+1)+"/montant").text = "-1"
--- Server/Server.gd
+++ Server/Server.gd
@@ -201,7 +201,7 @@ func _close_request_jeu (id, code, reason):
 	print("SERVEUR PARTIE : Client %d disconnecting with code: %d, reason: %s" % [id, code, reason])
 	
 func _disconnected_jeu (id, was_clean = false, serveur_jeu = null):
-	serveur_jeu.list_joueurs.erase(id)
+	supprimer_joueur(id, serveur_jeu)
 	print("SERVEUR PARTIE : Client %d disconnected, clean: %s" % [id, str(was_clean)])
 
 func _on_data_jeu(id_client, serveur_jeu):
@@ -502,3 +502,10 @@ func _on_Server_fin_partie(code):
 			serveurs_partie.remove(i)
 			return
 	print("Serveur de partie non trouvé pour finir")
+
+func supprimer_joueur(id, serveur_jeu):
+	serveur_jeu.list_joueurs.erase(id)
+	var structure = Structure.new()
+	structure.set_requete_cache_joueur(serveur_jeu.list_joueurs.find(id))
+	for client in serveur_jeu.list_joueurs: # Broadcast sur tous les joueurs
+		envoyer_message(serveur_jeu.socket, structure.to_bytes(), client)
