08540b9d6621b5329d03cc24d1e18f54eee627b7
Neiko DAMOI <neiko.damoi@etu.unistra.fr>
2021-05-08 18:45:18 +0200

    affichage panel joueur

--- Client/Scripts/infobox.gd
+++ Client/Scripts/infobox.gd
@@ -1,4 +1,5 @@
 extends Container
+
 onready var panel = get_node("../../panelPlayer")
 onready var infos = get_node("../../panelInfos")
 onready var case1 = panel.get_node("ColorRect/case")
@@ -22,6 +23,22 @@ var type_button = 0
 #var prop = 0
 var ind = 0
 
+var prop_indices = [[1,3],[5,15,25,35],[6,8,9],[11,13,14],[12,27], [16,18,19],[21,23,24],[26,28,29],[31,33,34],[37,39]]
+var prop_names = [["Institut Le Bel", "Service des sports"]
+				 ,["Arrêt galia", "Arrêt Esplanade", "Arrêt Observatoire", "Arrêt Place de l'étoile"], 
+				 ["Atrium", "Faculté de Chimie", "Le Patio"], 
+				 ["Faculté de Géographie", "Faculté de Médecine", "Faculté Physique et Ingénierie"], ["Électricité de Strasbourg", "Compagnie des eaux"], ["Bibliothèque BNU", "Observatoire", "Resto U"], ["INSA", "Plateforme de Biologie", "Le Portique"], ["Opéra national", "Parlement Européen", "Stade de la Meinau"], ["Faculté de droit", "Faculté des sciences historiques", "Faculté de pharmacie"]
+				 ,["Faculté de Gestion et d'Économie", "UFR Maths info"]]
+var prop_loyer = [[[2,10,30,90,160,250], [4,20,60,180,320,450]], 
+				  [[25,50,100,200], [25,50,100,200], [25,50,100,200], [25,50,100,200]], 
+				  [[6,30,90,270,400,550], [6,30,90,270,400,550], [8,40,100,300,450,600]], 
+				  [[10,50,150,450,625,750], [10,50,150,450,625,750], [12,60,180,500,700,900]], 
+				  [[0,0,0,0,0,0], [0,0,0,0,0,0]], [[14,70,200,550,750,950], [14,70,200,550,750,950], [16,80,220,600,800,1000]], [[18,90,250,700,875,1050], [18,90,250,700,875,1050],[18,90,250,700,875,1050], [20,100,300,750,925,1100]], [[22,110,330,800,975,1150], [22,110,330,800,975,1150], [24,120,360,850,1025,1200]], [[26,130,390,900,1100,1275], [26,130,390,900,1100,1275], 
+				   [28,150,450,1000,1200,1400]], [[35,175,500,1100,1300,1500], [50,200,600,1400,1700,2000]]]
+
+var prop_prix = [[60, 60], [200, 200, 200, 200], [100, 100, 120], 
+				 [140, 140, 160], [150, 150], [180, 180, 200], 
+				 [220, 220, 240], [260, 260, 280], [300, 300, 320], [350, 400]]
 signal sig_stats(infobox,player)
 
 # Called when the node enters the scene tree for the first time.
@@ -35,6 +52,7 @@ func prop_pressed(color, indice, total, contraste, nb_prop):
 	case4.set_normal_texture(color)
 	
 	get_tree().get_root().get_node("jeu").joueur.prop = nb_prop
+
 	self.ind = indice
 	
 	for i in range(4):
@@ -51,11 +69,12 @@ func prop_pressed(color, indice, total, contraste, nb_prop):
 		cases[indice].get_node("hyp").hide()
 	
 	get_node("../../panelPlayer/ColorRect/money").text = get_tree().get_root().get_node("jeu").joueur.argent[int(name[7])-1]
-	
+	case_pressed(indice)
 	panel.show()
 
 func _on_prop1_pressed():
 	prop_pressed(brun, 0, 2, 0.4, 1)
+	
 
 func _on_prop3_pressed():
 	prop_pressed(brun, 1, 2, 0.4, 3)
@@ -232,3 +251,46 @@ func _on_jailbreak_pressed():
 	var id = get_tree().get_root().get_node("jeu").joueur.id+1
 	if("infobox"+str(id) == name):
 		get_tree().get_root().get_node("jeu").carte_sortie_prison()
+
+func case_pressed(indice):
+	var res_col = 0
+	var prop = get_tree().get_root().get_node("jeu").joueur.prop
+	for col in range(0,len(prop_indices)):
+		for i in range(0,len(prop_indices[col])):
+			if( prop == prop_indices[col][i]):
+				res_col = col
+				
+	get_node("../../panelPlayer/ColorRect/loyers/nom_propriete").text= prop_names[res_col][indice]
+	get_node("../../panelPlayer/ColorRect/loyers/loyer").text = "loyer nu: " +str(prop_loyer[res_col][indice][0])
+	get_node("../../panelPlayer/ColorRect/loyers/1maison").text = "avec 1 maison: " + str(prop_loyer[res_col][indice][1])
+	get_node("../../panelPlayer/ColorRect/loyers/2maison").text = "avec 2 maison: " + str(prop_loyer[res_col][indice][2])
+	get_node("../../panelPlayer/ColorRect/loyers/3maison").text = "avec 3 maison: " + str(prop_loyer[res_col][indice][3])
+	get_node("../../panelPlayer/ColorRect/loyers/4maison").text = "avec 4 maison: " + str(prop_loyer[res_col][indice][4])
+	get_node("../../panelPlayer/ColorRect/loyers/hotel").text = "avec 1 hotel: " + str(prop_loyer[res_col][indice][5])
+	var prix_const = 0
+	if(prop < 10):
+		prix_const = 50
+	elif(prop < 20):
+		prix_const = 100
+	elif(prop < 30):
+		prix_const = 150
+	else:
+		prix_const = 200
+	get_node("../../panelPlayer/ColorRect/loyers/prixparmaison").text = "prix maison: " + str(prix_const)
+	get_node("../../panelPlayer/ColorRect/loyers/prixparhotel").text = "prix hotel: " + str(prix_const)
+	get_node("../../panelPlayer/ColorRect/loyers/valeur_hypo").text = "Valeur hypothecaire: " + str(prop_prix[res_col][indice])
+	
+func _on_case_pressed():
+	case_pressed(0)
+
+
+func _on_case2_pressed():
+	case_pressed(1)
+
+
+func _on_case3_pressed():
+	case_pressed(2)
+
+
+func _on_case4_pressed():
+	case_pressed(3)
