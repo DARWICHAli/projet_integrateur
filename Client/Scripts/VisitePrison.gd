274679c759788629eaaef3a599bd374c773d7b81
Jamel <jessalhi@etu.unistra.fr>
2021-05-11 20:43:09 +0200

    Nettoyage master

--- Client/Scripts/VisitePrison.gd
+++
@@ -1,9 +0,0 @@
-extends "Case.gd"
-
-
-
-func _ready():
-	pass 
-
-func _on_VisitePrison_body_entered(body):
-	print("VISITE EN PRISON")
--- README.md
+++
@@ -1,3 +0,0 @@
-# projet_integrateur_df
-
-Projet intégrateur L3IS6 2021, équipe DF.
