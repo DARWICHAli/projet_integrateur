cbc58677f96a0b6c6261f20e974e9cc1df817968
Jamel <jessalhi@etu.unistra.fr>
2021-05-11 06:38:33 +0200

    fix prix

--- Server/Serveur_partie.gd
+++ Server/Serveur_partie.gd
@@ -112,7 +112,7 @@ func init_plateau():
 	for i in range(40):
 		plateau[i].indice = i
 		if i == 0:
-			plateau[i].set_depart(500)
+			plateau[i].set_depart(1500)
 		elif i == 10:
 			plateau[i].set_prison()
 		elif i == 30:
